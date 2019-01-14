08c98e7e28e780311e3d7b522f4b11319d38dfb2
Tygydyk <32385649+Tygydyk@users.noreply.github.com>
2019-01-14 22:39:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,31 @@
-#include "stack.h"
 #include <iostream>
-#include <stdlib.h>
+#include <exception>
+#include "Stack.h"
 
 using namespace std;
 
 int main()
 {
-	Stack <int> *st = new Stack<int>;
-	st->push(4);
-	cout << st->head() << endl;
-	int a = 3;
-	st->push(a);
-	cout << st->head() << endl;
-	st->pop();
-	cout << st->head() << endl;
-	system("pause");
-	return 0;
+	try {
+		Stack<int> st;
+		st.push(1);
+		st.push(2);
+		st.push(5);
+
+		cout << st.head() << endl;
+
+		int a = 4;
+
+		st.push(a);
+
+		cout << st.head() << endl;
+
+		st.pop();
+
+		cout << st.head() << endl;
+
+	}
+	catch (const exception &e) {
+		cerr << " EXCEPTION: " << e.what() << endl;
+	}
 }
